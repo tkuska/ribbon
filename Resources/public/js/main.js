50ec305dba4a599d42111b155089d3a216cccbf8
Tomasz Kuśka <tomasz.kuska@gmail.com>
2013-11-14 22:03:50 +0100

    zmieniony JS, umożliwiający usuwanie obiektów z poziomu listy obiektów

--- Resources/public/js/main.js
+++ Resources/public/js/main.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
+$(document).ready(function() {
     //obsługa kliknięć na buttonach
     $('div[data-action]').click(function(e) {
-        if(!$(this).hasClass('disabled')){
+        if (!$(this).hasClass('disabled')) {
             var akcja = $(e.currentTarget).data('action');
             if (akcja.length > 3) {
-                switch(akcja){
+                switch (akcja) {
                     case 'submit-form':
                         $('.submit_btn').trigger('click');
                         break;
@@ -12,17 +12,47 @@ $(document).ready(function(){
                         history.go(-1);
                         break;
                     case 'object-delete':
-                        $('.delete_btn').trigger('click')
-                        break;                    
+                        noty({
+                            text: 'Na pewno chcesz usunąć ten obiekt?',                            
+                            type: 'confirm',
+                            layout: 'topCenter',
+                            buttons: [
+                                {
+                                    text: 'Usuń', 
+                                    onClick: function($noty) {
+                                        if($('.records_list')){
+                                            var new_action = $('.delete_btn').parent().parent().parent().attr('action').replace('recordid', $('.row_selected').attr('id'));
+                                            $('.delete_btn').parent().parent().parent().attr('action', new_action);
+                                            $('.delete_btn').trigger('click');
+                                        }
+                                        else{
+                                            $('.delete_btn').trigger('click');
+                                        }
+                                    }
+                                },
+                                {
+                                    text: 'Anuluj', 
+                                    onClick: function($noty) {
+                                        $noty.close();
+                                    }
+                                }
+                            ],   
+                            callback: {
+                                onShow: function() {
+                                    $('button', '.noty_buttons').button();
+                                }
+                            }
+                        });
+                        break;
                     default:
-                        var parameters = $('.row_selected').data('parameters')
-                        if(parameters === undefined){
+                        var parameters = $('.row_selected').data('parameters');
+                        if (parameters === undefined) {
                             var parameters = $(e.currentTarget).data('parameters');
-                        }  
-                        window.location = Routing.generate(akcja, parameters);                                       
+                        }
+                        window.location = Routing.generate(akcja, parameters);
                 }
             }
-                    
+
         }
     });
 });
